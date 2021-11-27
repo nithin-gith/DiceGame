3fd42e69e8e34a6b0d68f0f26739c99f50b968b1
Nithin Siriguppa <nithin.siriguppa@gmail.com>
2021-11-28 02:00:22 +0530

    version_1

---
+++ js/myscript.js
@@ -0,0 +1,57 @@
+var player_1 = prompt("Enter Name of Player-1");
+var player_2 = prompt("Enter Name of Player-2");
+
+document.getElementsByClassName("player-name")[0].innerHTML= player_1;
+document.getElementsByClassName("player-name")[1].innerHTML= player_2;
+
+
+function playAgain(){
+    var num1 = (Math.floor(Math.random()*6))+1;
+    var num2 = (Math.floor(Math.random()*6))+1;
+    
+    if (num1 > num2){
+        document.getElementsByTagName("h1")[1].innerHTML= player_1+" Wins";
+    }
+    else if (num2 > num1){
+        document.getElementsByTagName("h1")[1].innerHTML=player_2+ " Wins";    
+    }
+    else{
+        document.getElementsByTagName("h1")[1].innerHTML="It's a Tie";   
+    }
+    if(num1 == 1){
+        document.getElementById("first-image").src = "Images/DIce-1.png";
+    }
+    else if (num1 == 2){
+        document.getElementById("first-image").src = "Images/DIce-2.png";
+    }
+    else if (num1 == 3){
+        document.getElementById("first-image").src = "Images/DIce-3.png";
+    }
+    else if (num1 == 4){
+        document.getElementById("first-image").src = "Images/DIce-4.png";
+    }
+    else if (num1 == 5){
+        document.getElementById("first-image").src = "Images/DIce-5.png";
+    }
+    else if (num1 == 6){
+        document.getElementById("first-image").src = "Images/DIce-6.png";
+    }
+    if(num2 == 1){
+        document.getElementById("second-image").src = "Images/DIce-1.png";
+    }
+    else if (num2 == 2){
+        document.getElementById("second-image").src = "Images/DIce-2.png";
+    }
+    else if (num2 == 3){
+        document.getElementById("second-image").src = "Images/DIce-3.png";
+    }
+    else if (num2 == 4){
+        document.getElementById("second-image").src = "Images/DIce-4.png";
+    }
+    else if (num2 == 5){
+        document.getElementById("second-image").src = "Images/DIce-5.png";
+    }
+    else if (num2 == 6){
+        document.getElementById("second-image").src = "Images/DIce-6.png";
+    }
+}
